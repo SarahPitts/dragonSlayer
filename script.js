f6809d7f66d7c7b4f15bcd36b49dbf5bae2df588
Sarah Pitts <mccallsmedia@gmail.com>
2013-09-10 13:23:25 -0500

    adds setAttribute to header image

--- script.js
+++ script.js
@@ -6,14 +6,16 @@ totalDamage = 0;
 
 while(slaying) {
     if (youHit) {
-        document.getElementById('Hit').innerHTML="you hit the dragon!";
+        document.getElementById('Hit').innerHTML="You hit the dragon!";
         totalDamage += damageThisRound;
         if (totalDamage >= 4) {
-            document.getElementById('Result').innerHTML="You defeated the dragon";
+            document.getElementById('Result').innerHTML="You defeated the dragon.";
+            document.getElementById('image').setAttribute("src","images/bilbo.gif");
             slaying = false;
         }
         else {
-            document.getElementById('Result').innerHTML="The dragon defeated you";
+            document.getElementById('Result').innerHTML="The dragon defeated you.";
+            document.getElementById('image').setAttribute("src","images/dragonsGold.gif");
             slaying = false;
         }
     }
